407b0d79a24207ac62d767eefd6b5c167db0bd5c
Viacheslav-ua <atph@ukr.net>
2024-03-07 07:03:37 +0200

    first commit

---
+++ src/app/cs/page.tsx
@@ -0,0 +1,7 @@
+export default function CsPage() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">SC page</h1>
+    </div>
+  );
+}
---
+++ src/features/sidebar/sidebar.tsx
@@ -0,0 +1,7 @@
+
+
+export const Sidebar = () => {
+  return (
+    <div className="absolute top-0 left-0 text-xl w-[300px] h-screen bg-slate-300">sidebar</div>
+  )
+}
